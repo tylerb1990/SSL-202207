101bde97ea7104674669a7fe8a32a52534f03140
Tyler Bunker <tylerbunker@Tylers-MacBook-Pro.local>
2022-07-08 14:13:56 -0500

    updated server.js. Added Comments

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     error.status = 404;
     next(error);
 });
+//Middleware to send error nicely
 app.use((error, req, res, next) => {
     res.status(error.status || 500).json({
         error: {
