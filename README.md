cc1e8afdccfa41729aa9cb20cb5de1b7fe3f2a5d
Tyler Bunker <tylerbunker@Tylers-MacBook-Pro.local>
2022-07-08 14:00:18 -0500

    Initial Project Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NodeJS Test File
+## To run this project 
+### npm i
+### then
+### npm start
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+
+app.get("/", (req, res, next) => {
+    res.json({
+        message: "Did you GET it?"
+    })
+})
+
+
+// Middleware modules for error handling
+app.use((req, res, next) => {
+    const error = new Error("NOT FOUND");
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message, 
+            status: error.status,
+            method: req.method
+        }
+    });
+});
+
+
+app.listen(process.env.port, () => console.log(`Starting server on port ${process.env.port}`));
