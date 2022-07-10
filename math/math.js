e3c144e5415af05e6d67a7a4777591e7f1063bbf
Tyler Bunker <tylerbunker@Tylers-MacBook-Pro.local>
2022-07-10 17:11:38 -0500

    New test for math module. All tests passed.

---
+++ math/math.js
@@ -0,0 +1,20 @@
+// Add function
+const add = (x, y) => x+y;
+
+// Subtract function
+const subtract = (x, y) => x-y;
+
+// Divide function
+const divide = (x, y) => x/y;
+
+// Multiply function
+const multiply = (x, y) => x*y;
+
+// Square root function
+const sqrt = (x) => Math.sqrt(x);
+
+// Max funtion
+const max = (x, y) => Math.max(x,y);
+
+// Exports
+module.exports = {add, subtract, divide, multiply, sqrt, max}
